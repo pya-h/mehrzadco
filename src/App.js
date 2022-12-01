3abba00bec96c9f632a0006a4033142e3d95ee95
pya-h <p4ya.py@gmail.com>
2022-12-01 19:25:50 +0330

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { ToastContainer } from "react-toastify";
 import ContextProvider from "./context/ContextProvider";
-import AnimatedCursor from "react-animated-cursor";
 
 const App = () => {
     // this for animation
@@ -18,14 +17,7 @@ const App = () => {
 
     return (
         <ContextProvider>
-            <AnimatedCursor
-                innerSize={8}
-                outerSize={44}
-                color="255, 160, 1"
-                outerAlpha={0.3}
-                innerScale={0.7}
-                outerScale={0.8}
-            />
+
             <ScrollToTop />
             <AllRoutes />
             {/* End contact */}
